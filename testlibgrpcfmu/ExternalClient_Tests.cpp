e51069480284bdaf1583d898da6c8ed20b6de97e
Kenneth Lausdahl <lausdahl@eng.au.dk>
2015-09-04 12:54:07 +0200

    updated fmu tests to load and fmu testing

--- testlibgrpcfmu/ExternalClient_Tests.cpp
+++ testlibgrpcfmu/ExternalClient_Tests.cpp
@@ -1,61 +1,71 @@
 #include "gtest/gtest.h"
 
-
 #include <string>
 #include <iostream>
 
-extern "C" {
+extern "C"
+{
 #include "fmu-loader.h"
 }
 /*TEST(ExternalClient, system)
+ {
+ std::eexec("java -cp .:/home/parallels/Downloads/grpc-fmi/java/bin/:/home/parallels/Downloads/grpc-fmi/java/lib/netty-all-4.1.0.Beta5.jar:/home/parallels/Downloads/grpc-fmi/java/lib/grpc-all-0.8.0.jar:/home/parallels/Downloads/grpc-fmi/java/lib/guava-18.0.jar:/home/parallels/Downloads/grpc-fmi/java/lib/hpack-0.10.1.jar:/home/parallels/Downloads/grpc-fmi/java/lib/protobuf-java-3.0.0-alpha-3.1.jar rpc.Server");
+ }*/
+
+
+static FMU s_fmu;
+static bool s_libLoaded = false;
+
+/*
+TEST(LibraryLoad, loadsingle)
 {
-	std::eexec("java -cp .:/home/parallels/Downloads/grpc-fmi/java/bin/:/home/parallels/Downloads/grpc-fmi/java/lib/netty-all-4.1.0.Beta5.jar:/home/parallels/Downloads/grpc-fmi/java/lib/grpc-all-0.8.0.jar:/home/parallels/Downloads/grpc-fmi/java/lib/guava-18.0.jar:/home/parallels/Downloads/grpc-fmi/java/lib/hpack-0.10.1.jar:/home/parallels/Downloads/grpc-fmi/java/lib/protobuf-java-3.0.0-alpha-3.1.jar rpc.Server");
+	FMU fmu;
+	HMODULE h;
+
+	EXPECT_EQ(true, loadDll(FMULIB, &fmu, &h));
+}
+
+TEST(LibraryLoad, loaddouble)
+{
+	FMU fmu;
+	HMODULE h;
+
+	EXPECT_EQ(true, loadDll(FMULIB, &fmu, &h));
+	EXPECT_EQ(true, loadDll(FMULIB, &fmu, &h));
 }*/
 
-TEST(A, fmi2EnterInitializationMode)
+static FMU setup()
 {
+if(s_libLoaded)
+	return s_fmu;
+
 	FMU fmu;
-	//ExternalClient client("localhost:8980");
-	//EXPECT_EQ(ExternalClient::fmi2OK, client.fmi2EnterInitializationMode());
-	HMODULE h;
-int res=	loadDll(FMULIB,&fmu,&h);
-EXPECT_EQ(1,res);
-
-int r =fmu.enterInitializationMode(0);
-
-EXPECT_EQ(1,1);
-
-//dlclose(h);
-}
-
-
-TEST(A, instantiate)
-{
-	FMU fmu;
-
 	HMODULE h;
 
+	EXPECT_EQ(true, loadDll(FMULIB, &fmu, &h));
 
-int res=	loadDll(FMULIB,&fmu,&h);
-EXPECT_EQ(1,res);
-
-std::string instanceName = "my instance";
-	std::string fmuGUID = "dcnkjvnrevirehvljkfnvf";
-	std::string fmuResourceLocation = "/tmp";
-	//const char* callbackAddress = "localhost";
-	//int callbackPort=8080;
-	//bool visible = true;
-	//bool loggingOn = true;
-
-
-
-	fmi2Component r =fmu.instantiate(instanceName.c_str(),fmi2CoSimulation,fmuGUID.c_str(),fmuResourceLocation.c_str(),NULL,true,true);
-if(r==r)
-	int k=0;
-EXPECT_EQ(1,1);
-dlclose(h);
+	s_fmu = fmu;
+	s_libLoaded = true;
+	return fmu;
 }
 
+TEST(FMU, instantiate)
+{
+	FMU fmu = setup();
+	fmu.instantiate("A", fmi2CoSimulation,
+			"{348783748923}", ".", NULL, true, true);
+}
+
+TEST(FMU, fmi2EnterInitializationMode)
+{
+	FMU fmu = setup();
+	fmi2Component comp = fmu.instantiate("A", fmi2CoSimulation,
+			"{348783748923}", ".", NULL, true, true);
+
+	fmi2Status status = fmu.enterInitializationMode(comp);
+
+	EXPECT_EQ(fmi2OK, status);
+}
 
 
 
