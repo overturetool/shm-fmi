1411c1f70624d97ef73397964494cd4583679aee
Kenneth Lausdahl <lausdahl@eng.au.dk>
2015-09-09 15:53:30 +0200

    changed libgrpcfmu to use static linking to libgrpcfmi

--- coe-test/coe/debug-start-coe.sh
+++ coe-test/coe/debug-start-coe.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=../../libgrpcfmi
+#export LD_LIBRARY_PATH=../../libgrpcfmi
 java -Xdebug -Xrunjdwp:transport=dt_socket,address=8999,server=y -jar coe-0.0.1-SNAPSHOT-jar-with-dependencies.jar
