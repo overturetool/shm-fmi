4cbabc0cd5112ffe7eac064720368c0b5cec6a10
Kenneth Lausdahl <lausdahl@eng.au.dk>
2015-09-30 15:01:29 +0200

    removed grpc generation

--- grpcfmi-server/src/main/sh/generate.sh
+++ grpcfmi-server/src/main/sh/generate.sh
@@ -11,6 +11,3 @@ mkdir -p $OUTPUT
 echo Running protoc
 protoc --java_out=$OUTPUT --proto_path=$PROTO_ROOT $PROTO_ROOT/service.proto
 
-echo Running protoc with gRPC
-protoc --plugin=protoc-gen-grpc-java=/home/parallels/Downloads/grpc-java/compiler/build/binaries/java_pluginExecutable/protoc-gen-grpc-java \
-  --grpc-java_out=$OUTPUT --proto_path=$PROTO_ROOT $PROTO_ROOT/service.proto
