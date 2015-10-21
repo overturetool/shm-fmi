37a1d92e3b8ff039c9c81c4e884ed2041ceb8879
Kenneth Lausdahl <lausdahl@eng.au.dk>
2015-10-21 17:06:53 +0200

    updated script to download latest

--- coe-test/coe/start-coe.sh
+++ coe-test/coe/start-coe.sh
@@ -5,7 +5,7 @@ then
 echo "COE is present"
 else
 echo "Downloading COE, old fixed version for now"
-wget http://overture.au.dk/into-cps/development/Build-77_2015-10-16_00-00/coe-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+wget http://overture.au.dk/into-cps/development/latest/coe-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 fi
 
 java -jar coe-0.0.1-SNAPSHOT-jar-with-dependencies.jar
