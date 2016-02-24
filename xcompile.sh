6e36bf99e82050bf4faa81fe68156dda1815a323
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-02-24 20:43:29 +0100

    corrected indentation

--- xcompile.sh
+++ xcompile.sh
@@ -7,7 +7,7 @@ threads=4
 
 if [ -z "$SHM_DEPENDENCIES_ROOT" ]; 
 then echo "The dependency path 'SHM_DEPENDENCIES_ROOT' is now set. Please set it using 'export SHM_DEPENDENCIES_ROOT=/some/path/to/dependency/root'"; exit 1 
-#else echo "var is set to '$var'"; 
+		 #else echo "var is set to '$var'"; 
 fi
 
 echo "Building with dependencies from: '${SHM_DEPENDENCIES_ROOT}' using make -j${threads}"
@@ -22,13 +22,13 @@ function compileDarwin64
 		mkdir -p $B
 
 		JAVA_HOME=../third_party/jvm/darwin64 cmake  \
-							-B$B \
-							-H$1 \
-							-DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/osx-gcc.cmake` \
-							-DOSXCROSS_ROOT=$OSXCROSS_ROOT \
-							-DPROTOBUF_PROTOC_EXECUTABLE=/usr/bin/protoc \
-							-DPROTOBUF_INCLUDE_DIRS=$SHM_DEPENDENCIES_ROOT/darwin64/usr/protobuf/include/ \
-							-DPROTOBUF_LIBRARY=$SHM_DEPENDENCIES_ROOT/darwin64/usr/protobuf/lib/libprotobuf.a
+						 -B$B \
+						 -H$1 \
+						 -DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/osx-gcc.cmake` \
+						 -DOSXCROSS_ROOT=$OSXCROSS_ROOT \
+						 -DPROTOBUF_PROTOC_EXECUTABLE=/usr/bin/protoc \
+						 -DPROTOBUF_INCLUDE_DIRS=$SHM_DEPENDENCIES_ROOT/darwin64/usr/protobuf/include/ \
+						 -DPROTOBUF_LIBRARY=$SHM_DEPENDENCIES_ROOT/darwin64/usr/protobuf/lib/libprotobuf.a
 
 		make -C $B -j$threads
 
@@ -44,16 +44,16 @@ function compileWin32
 		mkdir -p $B
 
 		JAVA_HOME=../third_party/jvm/win32 cmake  \
-							-B$B \
-							-H$1 \
-							-DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/cmake-toolchains/Toolchain-Ubuntu-mingw32.cmake` \
-							-DPROTOBUF_INCLUDE_DIRS=$SHM_DEPENDENCIES_ROOT/win32/usr/protobuf/include/ \
-                                                        -DPROTOBUF_LIBRARY=$SHM_DEPENDENCIES_ROOT/win32/usr/protobuf/lib/libprotobuf.a
+						 -B$B \
+						 -H$1 \
+						 -DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/cmake-toolchains/Toolchain-Ubuntu-mingw32.cmake` \
+						 -DPROTOBUF_INCLUDE_DIRS=$SHM_DEPENDENCIES_ROOT/win32/usr/protobuf/include/ \
+             -DPROTOBUF_LIBRARY=$SHM_DEPENDENCIES_ROOT/win32/usr/protobuf/lib/libprotobuf.a
 
-# Do to a bug in gtest when compiling with MinGW we have to avoid building test code
-#               make -C $B -j$threads
-make -j$threads -C $B/libshmfmu
-make -j$threads -C $B/shmfmi-server
+		# Do to a bug in gtest when compiling with MinGW we have to avoid building test code
+		#               make -C $B -j$threads
+		make -j$threads -C $B/libshmfmu
+		make -j$threads -C $B/shmfmi-server
 
 }
 
@@ -67,16 +67,16 @@ function compileWin64
 		mkdir -p $B
 
 		JAVA_HOME=../third_party/jvm/win64 cmake  \
-							-B$B \
-							-H$1 \
-							-DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/cmake-toolchains/Toolchain-Ubuntu-mingw64.cmake` \
-							-DPROTOBUF_INCLUDE_DIRS=$SHM_DEPENDENCIES_ROOT/win64/usr/protobuf/include/ \
-                                                        -DPROTOBUF_LIBRARY=$SHM_DEPENDENCIES_ROOT/win64/usr/protobuf/lib/libprotobuf.a 
+						 -B$B \
+						 -H$1 \
+						 -DCMAKE_TOOLCHAIN_FILE=`readlink -f toolchains/cmake-toolchains/Toolchain-Ubuntu-mingw64.cmake` \
+						 -DPROTOBUF_INCLUDE_DIRS=$SHM_DEPENDENCIES_ROOT/win64/usr/protobuf/include/ \
+             -DPROTOBUF_LIBRARY=$SHM_DEPENDENCIES_ROOT/win64/usr/protobuf/lib/libprotobuf.a 
 
-# Do to a bug in gtest when compiling with MinGW we have to avoid building test code
-#		make -C $B -j$threads
-make -j$threads -C $B/libshmfmu
-make -j$threads -C $B/shmfmi-server
+		# Do to a bug in gtest when compiling with MinGW we have to avoid building test code
+		#		make -C $B -j$threads
+		make -j$threads -C $B/libshmfmu
+		make -j$threads -C $B/shmfmi-server
 
 
 }
@@ -89,10 +89,10 @@ function compileLinux64
 		mkdir -p $B
 
 		JAVA_HOME=../third_party/jvm/linux64 cmake  \
-							-B$B \
-							-H$1 \
-							-DPROTOBUF_INCLUDE_DIRS=$SHM_DEPENDENCIES_ROOT/linux64/usr/protobuf/include/ \
-                                                        -DPROTOBUF_LIBRARY=$SHM_DEPENDENCIES_ROOT/linux64/usr/protobuf/lib/libprotobuf.a
+						 -B$B \
+						 -H$1 \
+						 -DPROTOBUF_INCLUDE_DIRS=$SHM_DEPENDENCIES_ROOT/linux64/usr/protobuf/include/ \
+             -DPROTOBUF_LIBRARY=$SHM_DEPENDENCIES_ROOT/linux64/usr/protobuf/lib/libprotobuf.a
 
 		make -C $B -j$threads
 }
@@ -106,12 +106,12 @@ function compileLinux32
 		mkdir -p $B
 
 		JAVA_HOME=../third_party/jvm/linux64 cmake \
-							-B$B \
-							-H$1 \
-							-DLINK_FLAGS="-m32" \
-							-DCFLAGS="-m32" \
-							-DPROTOBUF_INCLUDE_DIRS=$SHM_DEPENDENCIES_ROOT/linux32/usr/protobuf/include/ \
-                                                        -DPROTOBUF_LIBRARY=$SHM_DEPENDENCIES_ROOT/linux32/usr/protobuf/lib/libprotobuf.a
+						 -B$B \
+						 -H$1 \
+						 -DLINK_FLAGS="-m32" \
+						 -DCFLAGS="-m32" \
+						 -DPROTOBUF_INCLUDE_DIRS=$SHM_DEPENDENCIES_ROOT/linux32/usr/protobuf/include/ \
+             -DPROTOBUF_LIBRARY=$SHM_DEPENDENCIES_ROOT/linux32/usr/protobuf/lib/libprotobuf.a
 
 		make -C $B -j$threads
 }
