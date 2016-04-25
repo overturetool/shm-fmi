3a149b0f9e839c4e0d5531b75950c7016e11867c
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-04-26 01:04:22 +0200

    added debug print for launched client

--- libshmfmu/fmu.cpp
+++ libshmfmu/fmu.cpp
@@ -142,6 +142,8 @@ extern "C" fmi2Component fmi2Instantiate(fmi2String instanceName,
 		return NULL;
 	}
 
+	printf("FMU Server client create, hosting SHM with raw key: %s\n", url);
+
 	FmuContainer *container = new FmuContainer(client, instanceName, functions,
 			launcher);
 
