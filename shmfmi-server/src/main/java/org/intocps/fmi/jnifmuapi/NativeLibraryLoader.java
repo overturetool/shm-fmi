1e25176d5817d5a3947aaf6cba6a6fdba18328f6
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-05-19 12:44:35 +0200

    changed temp folder creation to use Java 7
    fixes https://github.com/into-cps/INTO-CPS_Application/issues/8

--- shmfmi-server/src/main/java/org/intocps/fmi/jnifmuapi/NativeLibraryLoader.java
+++ shmfmi-server/src/main/java/org/intocps/fmi/jnifmuapi/NativeLibraryLoader.java
@@ -18,13 +18,14 @@ import org.slf4j.LoggerFactory;
 public class NativeLibraryLoader
 {
 	final static Logger logger = LoggerFactory.getLogger(NativeLibraryLoader.class);
-	final static String JAVA_IO_TEMP_PROPERTY = "java.io.tmpdir";
 
 	static Map<String, File> loadedLibs = new HashMap<String, File>();
 
 	public static File getTempFolder()
 	{
-		return new File(System.getProperty(JAVA_IO_TEMP_PROPERTY));
+		TempDirectory tempDir = new TempDirectory("jnifmuapi");
+		tempDir.deleteOnExit();
+		return tempDir.getPath().toFile();
 	}
 
 	public File loadLibrary(String relativeLibraryPath) throws Exception
@@ -34,12 +35,14 @@ public class NativeLibraryLoader
 			return loadedLibs.get(relativeLibraryPath);
 		}
 
-		logger.debug("Extracting library from: {}",relativeLibraryPath);
+		logger.debug("Extracting library from: {}", relativeLibraryPath);
 		File lib = extractNativeLibrary(getTempFolder(), relativeLibraryPath);
-		
-		if(lib==null)
+		logger.debug("Extracted library: {}", lib);
+
+		if (lib == null)
 		{
-			throw new FileNotFoundException("Unable to find the native lib: "+relativeLibraryPath);
+			throw new FileNotFoundException("Unable to find the native lib: "
+					+ relativeLibraryPath);
 		}
 
 		String libname = lib.getName();
@@ -50,8 +53,9 @@ public class NativeLibraryLoader
 		{
 			libname = libname.substring(0, libnameDotIndex);
 		}
-		
-		if(libname.startsWith("lib")&& (System.getProperty("os.name").equalsIgnoreCase("linux")|| System.getProperty("os.name").toLowerCase().startsWith("mac")))
+
+		if (libname.startsWith("lib")
+				&& (System.getProperty("os.name").equalsIgnoreCase("linux") || System.getProperty("os.name").toLowerCase().startsWith("mac")))
 		{
 			libname = libname.substring(3);
 		}
@@ -67,7 +71,7 @@ public class NativeLibraryLoader
 	}
 
 	public File extractNativeLibrary(File outputFolder,
-			String relativeLibraryPath)
+			String relativeLibraryPath) throws IOException
 	{
 		String name = relativeLibraryPath;
 
@@ -77,8 +81,7 @@ public class NativeLibraryLoader
 		{
 			name = name.substring(slashIndex + 1);
 		}
-		
-		
+
 		String osName = System.getProperty("os.name");
 
 		int index = osName.indexOf(' ');
@@ -86,30 +89,19 @@ public class NativeLibraryLoader
 		{
 			osName = osName.substring(0, index);
 		}
-		
-		outputFolder = new File(outputFolder,"fmi-shm-" +osName + "-"
+
+		outputFolder = new File(outputFolder, "fmi-shm-" + osName + "-"
 				+ System.getProperty("os.arch"));
 		outputFolder.mkdirs();
 
-		File library = new File(outputFolder, name );
+		File library = new File(outputFolder, name);
 
 		InputStream in = this.getClass().getClassLoader().getResourceAsStream(relativeLibraryPath);
 		if (in != null)
 		{
-			try
-			{
-				Files.copy(in, Paths.get(library.getAbsolutePath()), StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(in, Paths.get(library.getAbsolutePath()), StandardCopyOption.REPLACE_EXISTING);
 
-				// request the file to be deleted on exit
-				library.deleteOnExit();
-
-				return library;
-			} catch (IOException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-
-			}
+			return library;
 		}
 		return null;
 	}
@@ -145,28 +137,4 @@ public class NativeLibraryLoader
 		usrPathsField.set(null, newPaths);
 	}
 
-	// public static void Load(File jarFile, File destDir, String lib)
-	// throws IOException
-	// {
-	// JarFile jar = new JarFile(jarFile);
-	// Enumeration<?> enumEntries = jar.entries();
-	// while (enumEntries.hasMoreElements())
-	// {
-	// JarEntry file = (JarEntry) enumEntries.nextElement();
-	// File f = new File(destDir, file.getName());
-	// if (file.isDirectory())
-	// { // if its a directory, create it
-	// f.mkdir();
-	// continue;
-	// }
-	// InputStream is = jar.getInputStream(file); // get the input stream
-	// FileOutputStream fos = new FileOutputStream(f);
-	// while (is.available() > 0)
-	// { // write contents of 'is' to 'fos'
-	// fos.write(is.read());
-	// }
-	// fos.close();
-	// is.close();
-	// }
-	// }
 }
