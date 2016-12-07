319efecb5555e7c0a0e8612c81de09e64082d522
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-12-07 13:54:31 +0100

    wrong past. Now the proper flag is used

--- libshmfmu/JavaLauncher.cpp
+++ libshmfmu/JavaLauncher.cpp
@@ -119,7 +119,7 @@ int JavaLauncher::launch() {
                      NULL,  // Process handle not inheritable
                      NULL,  // Thread handle not inheritable
                      FALSE,  // Set handle inheritance to FALSE
-                     (m_visible ? 0 : visible),  // No creation flags
+                     (m_visible ? 0 : CREATE_NO_WINDOW),  // No creation flags
                      NULL,          // Use parent's environment block
                      m_workingDir,  // Use parent's starting directory
                      &si,           // Pointer to STARTUPINFO structure
