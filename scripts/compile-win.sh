6de5615461ba00715fda59632b86fa1d4aee6156
Kenneth Lausdahl <kgl@mjolner.dk>
2020-05-20 13:36:49 +0200

    added windows integration test with wine

--- scripts/compile-win.sh
+++ scripts/compile-win.sh
@@ -62,4 +62,6 @@ fi
 
 #./$executer bash -c "find \"builds/win-${platform}\" -name \"*.so\" -exec echo {} \; -exec file {} \; -exec ldd {} \;"
 
+docker run -it -v $(pwd):/work thawsystems/wine-stable wine /work/builds/win${platform}/testfmu-integration/testfmu-integration.exe
+
 ./$executer  bash -c "find /work -name \"*.dll\" -exec echo {} \; -exec bash -c \"objdump -p {} | grep 'DLL Name:'\" \;"
